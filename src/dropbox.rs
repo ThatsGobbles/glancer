ad2bb7664510de3075bd73a93a8b76882e38840b
Mark LeMoine <thatsgobbles@gmail.com>
2019-09-20 16:54:51 -0700

    Dropbox image listing now working

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -1,29 +1,57 @@
 use reqwest::Client;
-use reqwest::Response;
+use serde::Deserialize;
 
-pub struct HTTPListResponse(Vec<ImageMetadata>);
+#[derive(Debug)]
+pub enum Error {
+    ListingFetch(reqwest::Error),
+    ListingParse(reqwest::Error),
+}
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Self::ListingFetch(ref err) => write!(f, "error when listing files: {}", err),
+            Self::ListingParse(ref err) => write!(f, "error when deserializing payload: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            Self::ListingFetch(ref err) => Some(err),
+            Self::ListingParse(ref err) => Some(err),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct ImageListing {
+    entries: Vec<ImageMetadata>,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct ImageMetadata {
-    path: String,
+    path_lower: String,
     name: String,
 }
 
 pub struct HTTPTempLink(String);
 
-pub fn list_images() -> Response {
+pub fn get_image_listing() -> Result<ImageListing, Error> {
     let client = Client::new();
-    let response = {
-        client
-            .post("https://api.dropboxapi.com/2/files/list_folder")
-            .header("Authorization", "Bearer KHopaZZMKCIAAAAAAAAH5ojsxHMYzWlrvd_6wCTguwkRbQCPNTt6NSL-C5YpDNUX")
-            .header("Content-Type", "application/json")
-            // .header("Dropbox-Api-Select-User", "")
-            .body(r#"{ "path": "" }"#)
-            .send()
-            .unwrap()
-    };
+    let mut response = client
+        .post("https://api.dropboxapi.com/2/files/list_folder")
+        .header("Authorization", "Bearer KHopaZZMKCIAAAAAAAAH5ojsxHMYzWlrvd_6wCTguwkRbQCPNTt6NSL-C5YpDNUX")
+        .header("Content-Type", "application/json")
+        .body(r#"{ "path": "" }"#)
+        .send()
+        .map_err(Error::ListingFetch)?
+    ;
 
-    response
+    let http_list_response: ImageListing = response.json().map_err(Error::ListingParse)?;
+
+    Ok(http_list_response)
 }
 
 #[cfg(test)]
@@ -31,11 +59,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_list_images() {
-        let mut response = list_images();
-        println!("STATUS: {}", response.status());
-        println!("TEXT: {:?}", response.text().unwrap());
-        // let json_str: String = response.json().unwrap();
-        // println!("{}", json_str);
+    fn test_get_image_listing() {
+        let response = get_image_listing().unwrap();
+        println!("{:?}", response.entries);
     }
 }
